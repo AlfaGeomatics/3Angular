942591dedabd9b841acb902435a2d852961d2ff7
AlfaGeomatics <60299834+AlfaGeomatics@users.noreply.github.com>
2020-08-24 11:33:42 -0600

    Add files via upload

---
+++ data/200824Puntosdeinters_3.js
@@ -0,0 +1,8 @@
+var json_200824Puntosdeinters_3 = {
+"type": "FeatureCollection",
+"name": "200824Puntosdeinters_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Nombre": "Terreno de interés" }, "geometry": { "type": "Point", "coordinates": [ -89.208396575643576, 13.737554513193066 ] } }
+]
+}
